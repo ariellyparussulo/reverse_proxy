9f5915ef6bd1dfd0265e0780e331c320e550e094
Thiago Sanches <tsigwt@gmail.com>
2020-02-13 21:45:08 -0300

    Update main.py
    Co-authored-by: ariellyparussulo <ariellyblabla@gmail.com>

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import sys
 import yaml
 
+global configFile
+
 if len(sys.argv) is not 2:
     print('Please, pass the configuration file of this server.')
     sys.exit(0)
 
-CONFIG_FILE=sys.argv[1]
+CONFIG_FILE_PATH = sys.argv[1]
 
-with open(CONFIG_FILE) as f:
+with open(CONFIG_FILE_PATH) as f:
+    configFile = yaml.load(f, Loader=yaml.FullLoader)
 
-    data = yaml.load(f, Loader=yaml.FullLoader)
-    print(data)
+print("data %s" % configFile)
+
