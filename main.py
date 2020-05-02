1fe7c50354e9191ea4d8d88ac4c71ba253c01739
Thiago Sanches <tsigwt@gmail.com>
2020-05-01 22:01:55 -0300

    Add a simple listener on the main port

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ This program was created to run a reverse proxy.
 
 import sys
 import yaml
+import socket
+import sys
 from parsers import global_parser
 
 if len(sys.argv) != 2:
@@ -11,9 +13,23 @@ if len(sys.argv) != 2:
     sys.exit(0)
 
 YAML_FILE = sys.argv[1]
+HOST = ''
+PORT = None
 
 with open(YAML_FILE) as f:
     YAML_FILE = yaml.load(f, Loader=yaml.FullLoader)
 
 for config_node in YAML_FILE:
+    if config_node == "main":
+        PORT = YAML_FILE[config_node]['port']
     global_parser.GlobalParser().parse(config_node, YAML_FILE[config_node])
+
+skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+skt.bind((HOST, PORT))
+skt.listen()
+
+while True:
+    conn, addr = skt.accept()
+    print ('Client connected: ' + addr[0])
+
+skt.close()
